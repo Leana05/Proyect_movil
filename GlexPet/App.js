c7e4138aaa9643c1e24ba16503fb9fb2666b5b0e
Gustavo <gustavelasquez1@gmail.com>
2024-05-17 17:34:24 -0500

    Espero no tirarme el proyecto :o

--- GlexPet/App.js
+++ GlexPet/App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './src/components/Dashboard';
+import Home from './src/components/Home';
 
 export default function App() {
+
+  // Definimos la variable Stack que no permitirá usar los atribustos de nuestra barra de navegación
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // // Este es otro método de Navegación
+    // <NavigationContainer style={styles.container}>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name='Home' component={Home} />
+    //     <Stack.Screen name='Dashboard' component={Dashboard} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    // Esta es la TabBar o la barra de Navegación
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='User' component={'User'} />
+        <Tab.Screen name='AddPets' component={'AddPets'} />
+        <Tab.Screen name='Program' component={'Program'} />
+        <Tab.Screen name='Cart' component={'Cart'} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
