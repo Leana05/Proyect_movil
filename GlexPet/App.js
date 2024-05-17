8bc401a39fdf3498fe7ae708f58a874bef242af7
Gustavo <gustavelasquez1@gmail.com>
2024-05-17 18:35:45 -0500

    Se agregó la TabBar. falta acomodarla a la pantalla correctamente

--- GlexPet/App.js
+++ GlexPet/App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './src/components/Dashboard';
+import Home from './src/screens/Home';
+import AddPets from './src/screens/AddPets';
+import Cart from './src/screens/Cart';
+import User from './src/screens/User';
+import Program from './src/screens/Program';
 
 export default function App() {
+
+    // Definimos la variable Stack que no permitirá usar los atribustos de nuestra barra de navegación
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // // Este es otro método de Navegación
+    // <NavigationContainer style={styles.container}>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name='Home' component={Home} />
+    //     <Stack.Screen name='Dashboard' component={Dashboard} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    // Esta es la TabBar o la barra de Navegación
+    <NavigationContainer styles={styles.container}>
+      <Tab.Navigator>
+        <Tab.Screen name='AddPets' component={AddPets} />
+        <Tab.Screen name='Home' component={'Home'} />
+        <Tab.Screen name='User' component={'User'} />
+        <Tab.Screen name='Program' component={'Program'} />
+        <Tab.Screen name='Cart' component={Cart} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -19,6 +47,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     paddingHorizontal: 12,
-    paddingVertical:20
+    paddingVertical: 20,
   },
 });
